738ddec9fdd672e925d3fb12c1358c4617059938
Nsrri <nasrin.jafari@powercoders.orgr>
2024-03-31 10:38:22 +0200

    finish project

---
+++ Portfolio-Frontend-M293/src/scripts.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const filterItems = document.querySelectorAll(".filter-item");
+  const works = document.querySelectorAll(".filter-section");
+
+  function showWorks(category) {
+    works.forEach((work) => {
+      if (category === "All") {
+        work.style.display = "block";
+      } else {
+        const workCategory = work.dataset.category;
+        if (workCategory === category) {
+          work.style.display = "block";
+        } else {
+          work.style.display = "none";
+        }
+      }
+    });
+  }
+
+  showWorks("All");
+  document
+    .querySelector('.filter-item[data-category="All"]')
+    .classList.add("active");
+
+  filterItems.forEach((filterItem) => {
+    filterItem.addEventListener("click", function () {
+      const category = this.dataset.category;
+      console.log(category);
+      showWorks(category);
+
+      filterItems.forEach((item) => {
+        item.classList.remove("active");
+      });
+      this.classList.add("active");
+    });
+  });
+});
